a88f978aa35180e34ce09287596170f37f729a72
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-19 15:43:04 +0000

    interactive test

--- app/index.jsx
+++ app/index.jsx
@@ -1,28 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, TextInput } from "react-native";
 import { supabase } from "../utils/supabaseClient";
 
 function index() {
   const [data, setData] = useState(null);
+  const [distance, setDistance] = useState("100");
+  const [isPending, setIsPending] = useState(true);
 
-  useEffect(() => {
+  const invoke = () => {
+    setIsPending(true);
+
     supabase
       .rpc("nearby_markers", {
         longitude: -2.2490792,
         latitude: 53.4851459,
-        distance: 10,
+        distance: Number(distance) || 100,
       })
-      .then((res) => setData(res.data));
-  });
+      .then((res) => setData(res.data))
+      .finally(() => setIsPending(false));
+  };
+
+  useEffect(() => {
+    invoke();
+  }, []);
 
   return (
     <>
-      <Text>Hello World</Text>
-
-      <FlatList
-        data={data}
-        renderItem={(item) => <Text>{JSON.stringify(item)}</Text>}
+      <Text className="text-xl font-bold mb-4">Example: Postgis</Text>
+      <TextInput
+        className="border p-2 rounded mb-4"
+        value={distance}
+        onChangeText={setDistance}
+        onSubmitEditing={invoke}
       />
+      {isPending ? (
+        <Text>Pending...</Text>
+      ) : (
+        data?.length && (
+          <FlatList
+            data={data}
+            renderItem={({ item: { name, distance_meters } }) => (
+              <Text>
+                {name}: {distance_meters}m
+              </Text>
+            )}
+          />
+        )
+      )}
     </>
   );
 }
--- supabase/migrations/20241119113253_gis.sql
+++ supabase/migrations/20241119113253_gis.sql
@@ -1,15 +1,14 @@
 -- Initialize postgis
-DROP SCHEMA IF EXISTS gis CASCADE;
-CREATE SCHEMA IF NOT EXISTS gis;
-GRANT USAGE ON SCHEMA gis to anon;
+-- DROP SCHEMA IF EXISTS gis CASCADE;
 CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA gis;
+GRANT USAGE ON SCHEMA gis to anon;
 
 -- Recreate table
 DROP TABLE IF EXISTS gis_markers;
 CREATE TABLE gis_markers (
     marker_id uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
     name varchar NOT NULL,
-    location gis.geometry(POINT) NOT NULL
+    location gis.geography(POINT) NOT NULL
 );
 
 CREATE INDEX gis_markers_index ON gis_markers USING GIST (location);
@@ -17,7 +16,7 @@ CREATE INDEX gis_markers_index ON gis_markers USING GIST (location);
 -- Seed with data
 INSERT INTO gis_markers (name, location)
     VALUES  ('Manchester Museum', gis.st_point(-2.2368268, 53.4664686) ),
-            ('Emmeline Pankhurst Statue',gis.st_point(-2.243056 ,  53.477778)),
+            ('Emmeline Pankhurst Statue',gis.st_point(-2.243056 , 53.477778)),
             ('Mamucium Roman Fort Reconstruction', gis.st_point(-2.2588591, 53.4754896) ),
             ('Manchester Cathedral', gis.st_point(-2.2490792, 53.4851459) ),
             ('Alan Turing Memorial', gis.st_point(-2.2407774, 53.4767288) );
@@ -35,9 +34,10 @@ CREATE OR REPLACE FUNCTION nearby_markers(latitude float, longitude float, dista
     as $$
         SELECT  marker_id,
                 name,
-                gis.st_y(location) as latitude,
-                gis.st_x(location) as longitude,
-                gis.st_distance(location, gis.st_point(longitude, latitude)) as distance_meters
-                FROM gis_markers
-                    ORDER BY gis.st_dwithin(location, gis.st_point(longitude, latitude), distance)
+                gis.st_y(location::gis.geometry) as latitude,
+                gis.st_x(location::gis.geometry) as longitude,
+                gis.st_distance(location, gis.st_point(longitude, latitude)::gis.geography) as distance_meters
+        FROM gis_markers
+        WHERE gis.st_dwithin(location, gis.st_point(longitude, latitude)::gis.geography, distance)
+        ORDER BY distance_meters ASC
     $$;
